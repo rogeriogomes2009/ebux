c69f25bd87dbbea27c42fcdb2f5a05e0841d8c16
Rogério Gomes <95705053+rogeriogomes2009@users.noreply.github.com>
2022-03-02 23:49:09 -0300

    Up Sobre

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 import Menu from '../components/Menu'
+import { Container } from 'reactstrap'
+import Footer from '../components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Sobre = () => {
@@ -10,9 +12,26 @@ const Sobre = () => {
         <title>Sobre Nós - EBUX</title>
       </Head>
          <Menu />
-        
-          <a>Sobre Nós</a>
-        
+         <br/>
+         <br/>
+         <br/>
+         <Container fluid className="bg-dark text-white text-center pt-5">
+         <br/>
+          <h3 className="lead pt-2">Este app foi desenvolvido para experimentar habilidades como desenvolvedor Fullstack.<br/>
+          Caso você queira entrar em contato comigo, basta acessar qualquer dos link's apresentados<br/>
+          no rodapé da página. Caso queira contribuir com um livro que seja opensource, fique a vontade.<br/>
+          Para isso basta acessar o link INDIQUE UM LIVRO no topo da página.<br/> 
+          Grato por seu acesso!</h3>
+          <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+         <br/>
+        </Container>
+        <Footer />
     </div>
     )
 }
