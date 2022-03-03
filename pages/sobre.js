58e1a2692e1df75a146eaab5ec2766abf7b3154a
Rogério Gomes <95705053+rogeriogomes2009@users.noreply.github.com>
2022-03-02 23:52:16 -0300

    sobre upload

--- pages/sobre.js
+++ pages/sobre.js
@@ -18,6 +18,7 @@ const Sobre = () => {
          <Container fluid className="bg-dark text-white text-center pt-5">
          <br/>
           <h3 className="lead pt-2">Este app foi desenvolvido para experimentar habilidades como desenvolvedor Fullstack.<br/>
+          Nele foram usadas tecnologias e ferramentas como: NextJS, ReactJS, Express, Bootstrap, NodeJS, React-Icons, entre outros.
           Caso você queira entrar em contato comigo, basta acessar qualquer dos link's apresentados<br/>
           no rodapé da página. Caso queira contribuir com um livro que seja opensource, fique a vontade.<br/>
           Para isso basta acessar o link INDIQUE UM LIVRO no topo da página.<br/> 
@@ -27,7 +28,7 @@ const Sobre = () => {
          <br/>
          <br/>
          <br/>
-         <br/>
+         
          <br/>
          <br/>
         </Container>
